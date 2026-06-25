e4576c87ec80353f26779ce1200fce5223bd4943
Jacinto Martinez <jacintorobmartinez@gmail.com>
2026-06-25 16:57:57 -0300

    Edit estructura de la API

--- src/app.js
+++ src/app.js
@@ -5,9 +5,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("API funcionando correctamente");
+  res.json({
+    message: "API de alquiler de bicicletas funcionando correctamente",
+    version: "1.0.0",
+    endpoints: {
+      api: "/api",
+    },
+  });
 });
 
 module.exports = app;
